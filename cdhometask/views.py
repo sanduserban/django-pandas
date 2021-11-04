99b3a31e6bceb1e52d2e14795a81f033711ed889
sanduserban <serban@nops.io>
2021-11-04 16:06:47 +0200

    Add filtering and sorting

--- cdhometask/views.py
+++ cdhometask/views.py
@@ -2,25 +2,23 @@ import json
 from datetime import datetime
 
 import pandas
-from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
-
-# from rest_framework.viewsets import ModelViewSet
 
 from rest_framework.mixins import DestroyModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-
+from rest_framework.filters import OrderingFilter
+from cdhometask.filters import CandidateFilter
 from cdhometask.models import Candidate
 from cdhometask.serializers import CandidateSerializer, CandidateListSerializer, CandidateIngestSerializer
 
 
 class CandidateViewSet(DestroyModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['first_name', 'last_name', 'annual_income']
-    ordering_fields = ('first_name', 'last_name', 'dob')
     queryset = Candidate.objects.all()
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+    filterset_class = CandidateFilter
+    ordering_fields = '__all__'
 
     def get_serializer_class(self):
         if self.action == 'list':
